ba69890f8b70a215fe6a45bbbf2cbc3c6a87a432
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-25 23:47:34 -0400

    Update names.py

--- names/names.py
+++ names/names.py
@@ -38,7 +38,7 @@ for name_1 in names_1:
 """
 
 end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{(', '.join(duplicates))}\n\n")
+print(f"\n{len(duplicates)} duplicates:\n{(', '.join(duplicates))}\n")
 print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
