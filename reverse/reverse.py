a1dbb009eadb45d759287b315c4237117277ca58
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-30 20:15:02 -0400

    Update reverse.py

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -38,6 +38,20 @@ class LinkedList:
 
         return False
 
+    # Function using iteration reverses node order in a singly linked list
+    def reverse_list(self, node, prev):
+        if node:
+            current = self.head  # Set current to head for iterating through the linked list
+            # Loop while current is not None
+            while current:
+                next = current.get_next()  # Store a reference to the second node in the list in a variable next
+                current.set_next(prev)  # Store the current's next reverence to prev
+                prev = current  # Set prev with the reference to current
+                current = next  # Set the node referenced by next to current
+
+            self.head = prev  # This sets the last node to of the non-reversed list to the head of the new reversed list
+
+    """
     # Function reverses item order in a singly linked list
     def reverse_list(self, node, prev):
         # if node is not none proceed with reversing the list
@@ -58,6 +72,7 @@ class LinkedList:
 
             # if the list reversal is not complete then recursively call reverse_list
             return self.reverse_list(node, prev)
+    """
 
     def print_list(self):
         current = self.head
@@ -65,19 +80,21 @@ class LinkedList:
             print(current.get_value())
             current = current.get_next()
 
+
+# Created Linked list to run my own tests on the reverse_list function
 ll = LinkedList()
 
-ll.add_to_head(5)
-ll.add_to_head(8)
-ll.add_to_head(17)
-ll.add_to_head(25)
-ll.add_to_head(13)
-ll.add_to_head(14)
+ll.add_to_head(5)  # 5
+ll.add_to_head(8)  # 8 5
+ll.add_to_head(17)  # 17 8 5
+ll.add_to_head(25)  # 25 17 8 5
+ll.add_to_head(13)  # 13 25 17 8 5
+ll.add_to_head(14)  # 14 13 25 17 8 5
 
 print('\nNon-reversed List: ')
-ll.print_list()
+ll.print_list()  # Linked List should be: 14 13 25 17 8 5
 
-ll.reverse_list(Node(), None)
+ll.reverse_list(Node(), None)  # Call reverse list with an Empty Node and prev set as None
 
 print('\nReversed List: ')
-ll.print_list()
+ll.print_list()  # Linked List should be: 5 8 17 25 13 24
